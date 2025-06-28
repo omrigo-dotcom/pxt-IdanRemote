392066f48d178ff3a088a721b4249fd57cef7670
omrigo-dotcom <omrigo70@gmail.com>
2025-06-28 19:49:49 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,35 @@
 //% color=#007ACC icon="\u21BA" block="שלט"
 namespace RemoteControl {
-    /**
-     * בודק אם כפתור מחובר ל־%pin% לחוץ
-     */
-    //% block="האם כפתור מחובר ל־%pin% לחוץ"
-    export function isButtonPressed(pin: DigitalPin): boolean {
-        return pins.digitalReadPin(pin) == 0
+    enum RemoteButton {
+        //% block="למעלה"
+        Up = 0,
+        //% block="למטה"
+        Down = 1,
+        //% block="שמאלה"
+        Left = 2,
+        //% block="ימינה"
+        Right = 3
     }
 
+    let pinsMap = [DigitalPin.P0, DigitalPin.P1, DigitalPin.P2, DigitalPin.P3]
+    let wasPressed = [0, 0, 0, 0]
+
     /**
-     * קורא ערך מהפין
+     * מפעיל קוד כשנלחץ כפתור מסוים
      */
-    //% block="ערך דיגיטלי מהפין %pin%"
-    export function readPin(pin: DigitalPin): number {
-        return pins.digitalReadPin(pin)
+    //% block="כאשר נלחץ כפתור %btn"
+    export function onButtonPressed(btn: RemoteButton, handler: () => void) {
+        control.inBackground(function () {
+            while (true) {
+                let pin = pinsMap[btn]
+                if (pins.digitalReadPin(pin) == 0 && wasPressed[btn] == 0) {
+                    wasPressed[btn] = 1
+                    handler()
+                } else if (pins.digitalReadPin(pin) == 1) {
+                    wasPressed[btn] = 0
+                }
+                basic.pause(20)
+            }
+        })
     }
 }
