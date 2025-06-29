3165b4d94c818ea44b4f095bd83e34eb5bf538e0
omrigo-dotcom <omrigo70@gmail.com>
2025-06-29 09:58:13 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -85,13 +85,13 @@ namespace RemoteControl {
                         active = y > 800;
                         break;
                     case JoystickDirection.Left:
-                        active = x > 400;
+                        active = x > 800;
                         break;
                     case JoystickDirection.Right:
                         active = x < 200;
                         break;
                     case JoystickDirection.Center:
-                        active = x >= 400 && x <= 600 && y >= 200 && y <= 800;
+                        active = x >= 200 && x <= 600 && y >= 200 && y <= 800;
                         break;
                 }
 
